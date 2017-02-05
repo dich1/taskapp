f90930093a83fdd85c0402f31f8f2288cfaa3dc2
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2017-02-05 19:12:02 +0900

    first commit

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    /**
+     * UITableViewDatasourceプロトコルのメソッド(必須)
+     * データの数(=セルの数)を返すメソッド
+     * @param tableView
+     * @param section
+     **/
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    /**
+     * UITableViewDatasourceプロトコルのメソッド(必須)
+     * 各セルの内容を返すメソッド
+     * @param tableView
+     * @param indexPath
+     */
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能なcellを取得
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    /**
+     * UITableViewDelegateプロトコルのメソッド
+     * 各セルを選択した時に実行されるメソッド
+     * @param tableView
+     * @param indexPath
+     **/
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    /**
+     * UITableViewDelegateプロトコルのメソッド
+     * セルが削除可能なことを伝えるメソッド
+     * @param tableView
+     * @param indexPath
+     **/
+    public func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.delete
+    }
+    
+    /**
+     * UITableViewDelegateプロトコルのメソッド
+     * セルが削除可能なことを伝えるメソッド
+     * @param tableView
+     * @param editingStyle
+     * @param indexPath
+     **/
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
 }
 
